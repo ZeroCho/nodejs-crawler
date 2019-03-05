190fe091eb1623222ef4fa34320a47f5b5f56950
ZeroCho <zerohch0@gmail.com>
2019-03-05 21:56:50 +0900

    Add: 10.facebook-crawling

--- 10.facebook-crawling/index.js
+++ 10.facebook-crawling/index.js
@@ -27,11 +27,46 @@ const crawler = async () => {
     });
     await page.waitFor(1000);
     await page.keyboard.press('Escape');
-
-    await page.evaluate(() => {
-
-    });
-
+    let result = [];
+    while (result.length < 30) {
+      await page.waitForSelector('[id^=hyperfeed_story_id]:first-child');  // 아이디는 바뀔 수 있습니다.
+      const newPost = await page.evaluate(() => {
+        window.scrollTo(0, 0);
+        const firstFeed = document.querySelector('[id^=hyperfeed_story_id]:first-child');
+        const name = firstFeed.querySelector('.fwn.fcg') && firstFeed.querySelector('.fwn.fcg').textContent;
+        const content = firstFeed.querySelector('.userContent') && firstFeed.querySelector('.userContent').textContent;
+        const postId = firstFeed.id.split('_').slice(-1)[0];
+        return {
+          name,
+          content,
+          postId,
+        };
+      });
+      await page.waitFor(1000);
+      result.push(newPost);
+      console.log(result);
+      await page.waitForSelector('[id^=hyperfeed_story_id]:first-child ._666k a'); // 선택자는 바뀔 수 있습니다.
+      console.log('likebtn found');
+      const likeBtn = await page.$('[id^=hyperfeed_story_id]:first-child ._666k a');
+      await page.evaluate((like) => {
+        const sponsor = document.querySelector('[id^=hyperfeed_story_id]:first-child').textContent.includes('SpSpSononSsosoSredredSSS');
+        if (sponsor && like.getAttribute('aria-pressed') === 'true') {
+          like.click()
+        } else if (!sponsor && like.getAttribute('aria-pressed') === 'false') { // 좋아요가 안 되어 있으면
+          like.click()
+        }
+      }, likeBtn);
+      await page.waitFor(1000);
+      console.log('likebtn clicked');
+      await page.evaluate(() => {
+        const firstFeed = document.querySelector('[id^=hyperfeed_story_id]:first-child');
+        firstFeed.parentNode.removeChild(firstFeed);
+        window.scrollBy(0, 200);
+      });
+      await page.waitFor(1000);
+      console.log('tagRemoved');
+    }
+    console.log(result);
     // await page.click('#userNavigationLabel');
     // await page.waitForSelector('li.navSubmenu:last-child');
     // await page.waitFor(3000);

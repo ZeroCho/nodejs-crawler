6669091608f16c6cd046edfc6c04d937d67ddeb1
ZeroCho <zerohch0@gmail.com>
2019-03-07 13:40:16 +0900

    Add: 11.instagram-crawling draft

--- 11.instagram-crawling/index.js
+++ 11.instagram-crawling/index.js
@@ -7,32 +7,29 @@ dotenv.config();
 const crawler = async () => {
   try {
     await db.sequelize.sync();
-    const browser = await puppeteer.launch({ headless: false, args: ['--window-size=1920,1080', '--disable-notifications'] });
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: ['--window-size=1920,1080', '--disable-notifications'],
+      userDataDir: 'C:\Users\zerocho\AppData\Local\Google\Chrome\User Data'
+    });
     const page = await browser.newPage();
     await page.setViewport({
       width: 1080,
       height: 1080,
     });
-    await page.goto('https://facebook.com');
+    await page.goto('https://instagram.com');
+    // await page.waitFor(1000);
+    await page.waitForSelector('button.L3NKy');
+    await page.click('button.L3NKy');
+    await page.waitForNavigation();
+    await page.waitForSelector('#email');
     await page.type('#email', process.env.EMAIL);
     await page.type('#pass', process.env.PASSWORD);
-    await page.waitFor(1000);
-    await page.click('#loginbutton');
-    await page.waitForResponse((response) => {
-      return response.url().includes('login_attempt');
-    });
-    await page.keyboard.press('Escape');
-    await page.waitForSelector('textarea');
-    await page.click('textarea');
-    await page.waitForSelector('._5rpb > div');
-    console.log('found');
-    await page.click('._5rpb > div');
-    console.log('clicked');
-    await page.keyboard.type('인간지능 제로초봇 동작중... 페이스북은 제로초봇에게 점령당했습니다.');
-    await page.waitForSelector('._6c0o button');
-    await page.waitFor(5000);
-    await page.click('._6c0o button');
-
+    // await page.waitForResponse((response) => {
+    //   return response.url().includes('login_attempt');
+    // });
+    // await page.keyboard.press('Escape');
+    //
     // let result = [];
     // while (result.length < 10) {
     //   await page.waitForSelector('[id^=hyperfeed_story_id]:first-child');
